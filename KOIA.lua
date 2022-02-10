bbfe68eb67b656b51330e3e8d13df7b00dba0399
l-Ln <99279123+l-Ln@users.noreply.github.com>
2022-02-10 03:07:45 +0300

    Update KOIA.lua

--- KOIA.lua
+++ KOIA.lua
@@ -9630,10 +9630,10 @@ data = {
  },
  }
  }
-return LuaTele.sendText(msg_chat_id,msg_id,'*᥀︙رابط الحذف في جميع مواقع التواصل الاجتماعي*',"md",false, false, false, false, reply_markup)
+return LuaTele.sendText(msg_chat_id,msg_id,'*᥀︙رابط الحذف في جميع مواقع التواصل*',"md",false, false, false, false, reply_markup)
 end
 if text == 'السورس' or text == 'سورس' or text == 'يا سورس' or text == 'source' then
-photo = "https://t.me/DovePhotoo/5"
+
 local T =[[
 Welcome To Source [Source Dove](https://t.me/E5EEEF) .
 ]]
@@ -9650,7 +9650,7 @@ keyboard.inline_keyboard = {
  },
  }
 local msgg = msg_id/2097152/0.5
-https.request("https://api.telegram.org/bot"..Token.."/sendvideo?chat_id=" .. msg_chat_id .. "&photo="..photo.."&caption=".. URL.escape(T).."&reply_to_message_id="..msgg.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..Token.."/sendvideo?chat_id=" .. msg_chat_id .. '&photo=https://t.me/E5EEEF&caption='.. URL.escape(T).."&reply_to_message_id="..msgg.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 
 elseif text == 'الاوامر' then
 if not msg.Addictive then
